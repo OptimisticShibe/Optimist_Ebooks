6a71ccb5b1fa2ffe770e248b5b556209d015c318
Rafiq Ramadan <britishdragons123@gmail.com>
2021-12-02 23:08:50 -0600

    Delete user_accounts.js
    Removing user accounts from public repo

--- user_accounts.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    user: ['SharpiBees', 'RegalRegex', 'KaylaMod', 'KaylaMustDie', 'UnholyLykoi', 'CytoSprout', 'cerebroheart', 'tropicoastal', 'ManticoreAss', 'Azuiden', 'elektronxz', 'FurbyFarts'],
-    emoji: ['🐊', '🦌', '😎', '🍅', '🔪', '🌱', '💜', '🐴', '🤠', '🦎', '🔫', '🐔']
-}
